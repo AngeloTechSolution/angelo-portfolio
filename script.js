7adfedff9fd0fee64df0c12d1496e8fb32046dba
Noob <michealangelo715@gmail.com>
2025-03-11 17:31:22 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let navLinks = document.querySelectorAll(".navbar a");
+    navLinks.forEach(link => {
+        link.addEventListener("click", function() {
+            navLinks.forEach(nav => nav.classList.remove("active"));
+            this.classList.add("active");
+        });
+    });
+});
